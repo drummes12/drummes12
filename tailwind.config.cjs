34029f0731d02b239ecf42a0289667ec508df702
Esteban Gonzalez Drummes12 <daesgodu@gmail.com>
2023-11-08 16:00:37 -0500

    font drummes12

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,9 @@ module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
+      fontFamily: {
+        DMSans: 'DM Sans'
+      },
       backgroundImage: {
         'radial-gradient': 'radial-gradient(var(--tw-gradient-stops))',
       },
